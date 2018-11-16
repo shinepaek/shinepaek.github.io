5d09a097466c3fb26d559ebe60e5839d8c04c5f9
Shine Paek <s1paek@ucsd.edu>
2018-11-15 18:23:03 -0800

    trying to figure out login

--- login.js
+++ login.js
@@ -1,10 +1,10 @@
 // Name and Password from the register-form
-var name = document.getElementById('name');
+var name = document.getElementById('myname');
 var pw = document.getElementById('pw');
 
 // storing input from register-form
 function store() {
-    localStorage.setItem('name', name.value);
+    localStorage.setItem('myname', name.value);
     localStorage.setItem('pw', pw.value);
 }
 
@@ -12,17 +12,19 @@ function store() {
 function check() {
 
     // stored data from the register-form
-    var storedName = localStorage.getItem('name');
+    var storedName = localStorage.getItem('myname');
     var storedPw = localStorage.getItem('pw');
 
     // entered data from the login-form
     var userName = document.getElementById('userName');
     var userPw = document.getElementById('userPw');
 
+
+
     // check if stored data from register-form is equal to data from login form
-    if(userName.value !== storedName || userPw.value !== storedPw) {
+    if(userName.value != storedName || userPw.value != storedPw) {
         alert('ERROR');
     }else {
-        alert('You are loged in.');
+        alert('You are logged in.');
     }
 }
